2ff6f518820152d6ede9e06cf452d2db0f9167ba
sazuxerox <sazzadhossain@live.com>
2015-10-08 00:53:36 +0600

    Added basic routes, controllers and admin area

--- Dashboard/App_Start/RouteConfig.cs
+++ Dashboard/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Dashboard.Controllers;
 
 namespace Dashboard
 {
@@ -11,13 +12,11 @@ namespace Dashboard
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            var namespaces = new[] {typeof (PostsController).Namespace};
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Login", "login", new {controller = "Auth", action = "Login"}, namespaces);
+            routes.MapRoute("Home", "", new {controller = "Posts", action = "Index"}, namespaces);
         }
     }
 }
