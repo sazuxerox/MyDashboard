4f3d38a6e652d856dc8c7e0e8abe3f8e87ac5b2c
sazuxerox <sazzadhossain@live.com>
2015-11-03 23:03:34 +0600

    Index actions implemented on post. Paginations implemented.

--- Dashboard/Areas/Admin/Controllers/PostsController.cs
+++ Dashboard/Areas/Admin/Controllers/PostsController.cs
@@ -1,17 +1,33 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using Dashboard.Areas.Admin.ViewModels;
+using Dashboard.Models;
+using NHibernate.Linq;
 using Dashboard.Infrastructure;
 
 namespace Dashboard.Areas.Admin.Controllers
 {
+    //
+    // GET: /Admin/Posts/
+    [Authorize(Roles = "admin")]
+    [SelectedTab("posts")]
     public class PostsController : Controller
     {
-        //
-        // GET: /Admin/Posts/
-        [Authorize(Roles = "admin")]
-        [SelectedTab("posts")]
-        public ActionResult Index()
+        private const int PostsPerPage = 5; 
+        public ActionResult Index(int page = 1)
         {
-            return View();
+            var totalPostCount = Database.Session.Query<Post>().Count();
+
+            var currentPostPage = Database.Session.Query<Post>()
+                .OrderByDescending(c => c.CreatedAt)
+                .Skip((page - 1)*PostsPerPage)
+                .Take(PostsPerPage)
+                .ToList();
+
+            return View(new PostsIndex
+            {
+               Posts = new PagedData<Post>(currentPostPage, totalPostCount,page,PostsPerPage)
+            });
         }
 	}
 }
---
+++ Dashboard/Areas/Admin/ViewModels/Posts.cs
@@ -0,0 +1,10 @@
+﻿using Dashboard.Infrastructure;
+using Dashboard.Models;
+
+namespace Dashboard.Areas.Admin.ViewModels
+{
+    public class PostsIndex
+    {
+        public PagedData<Post> Posts { get; set; }
+    }
+}
