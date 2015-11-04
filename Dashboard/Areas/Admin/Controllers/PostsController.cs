c6a4f4e475cbea95b26cb1e5c3059ee856e25ba4
sazuxerox <sazzadhossain@live.com>
2015-11-05 01:04:41 +0600

    create, edit, trash, restore and delete features for post is created!

--- Dashboard/Areas/Admin/Controllers/PostsController.cs
+++ Dashboard/Areas/Admin/Controllers/PostsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using Dashboard.Areas.Admin.ViewModels;
 using Dashboard.Models;
@@ -13,11 +14,11 @@ namespace Dashboard.Areas.Admin.Controllers
     [SelectedTab("posts")]
     public class PostsController : Controller
     {
-        private const int PostsPerPage = 5; 
+        private const int PostsPerPage = 5;
+
         public ActionResult Index(int page = 1)
         {
             var totalPostCount = Database.Session.Query<Post>().Count();
-
             var currentPostPage = Database.Session.Query<Post>()
                 .OrderByDescending(c => c.CreatedAt)
                 .Skip((page - 1)*PostsPerPage)
@@ -26,8 +27,112 @@ namespace Dashboard.Areas.Admin.Controllers
 
             return View(new PostsIndex
             {
-               Posts = new PagedData<Post>(currentPostPage, totalPostCount,page,PostsPerPage)
+                Posts = new PagedData<Post>(currentPostPage, totalPostCount, page, PostsPerPage)
             });
         }
-	}
+
+        public ActionResult New()
+        {
+            return View("Form", new PostsForm
+            {
+                IsNew = true
+            });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var post = Database.Session.Load<Post>(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Form", new PostsForm
+            {
+                IsNew = false,
+                PostId = id,
+                Title = post.Title,
+                Slug = post.Slug,
+                Content = post.Content
+            });
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Form(PostsForm form)
+        {
+            form.IsNew = form.PostId == null;
+
+            if (!ModelState.IsValid)
+            {
+                return View(form);
+            }
+
+            Post post;
+            if (form.IsNew)
+            {
+                post = new Post
+                {
+                    CreatedAt = DateTime.Now,
+                    User = Auth.User
+                };
+            }
+            else
+            {
+                post = Database.Session.Load<Post>(form.PostId);
+                if (post == null)
+                {
+                    return HttpNotFound();
+                }
+                post.UpdatedAt = DateTime.Now;
+            }
+
+            post.Title = form.Title;
+            post.Slug = form.Slug;
+            post.Content = form.Content;
+
+            Database.Session.SaveOrUpdate(post);
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Trash(int id)
+        {
+            var post = Database.Session.Load<Post>(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            post.DeletedAt = DateTime.Now;
+            Database.Session.Update(post);
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            var post = Database.Session.Load<Post>(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            post.DeletedAt = DateTime.Now;
+            Database.Session.Delete(post);
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Restore(int id)
+        {
+            var post = Database.Session.Load<Post>(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            post.DeletedAt = null;
+            Database.Session.Update(post);
+            return RedirectToAction("Index");
+        }
+    }
 }
--- Dashboard/Areas/Admin/ViewModels/Posts.cs
+++ Dashboard/Areas/Admin/ViewModels/Posts.cs
@@ -1,4 +1,5 @@
-﻿using Dashboard.Infrastructure;
+﻿using System.ComponentModel.DataAnnotations;
+using Dashboard.Infrastructure;
 using Dashboard.Models;
 
 namespace Dashboard.Areas.Admin.ViewModels
@@ -6,5 +7,18 @@ namespace Dashboard.Areas.Admin.ViewModels
     public class PostsIndex
     {
         public PagedData<Post> Posts { get; set; }
+    }
+
+    public class PostsForm
+    {
+        public bool IsNew { get; set; }
+        public int? PostId { get; set; }
+        
+        [Required, MaxLength(128)]
+        public string Title { get; set; }
+        [Required, MaxLength(128)]
+        public string Slug { get; set; }
+        [Required, DataType(DataType.MultilineText)]
+        public string Content { get; set; }
     }
 }
